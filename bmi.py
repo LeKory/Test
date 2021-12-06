fd80f52b0a2d717e70757a64f6a9ca22d9832219
LeKory <95621175+LeKory@users.noreply.github.com>
2021-12-06 20:46:22 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -9,12 +9,12 @@ h = st.number_input('請輸入身高(M)？')
 confirm_input=st.button('請輸入確認')
 if confirm_input:
     bmi = BMI(w, h)
-    #st.print('BMI為', bmi)
+    st.write('BMI為', bmi)
     if (bmi < 18):
-        st.print('體重過輕')
+        st.write('體重過輕')
     elif (bmi < 24):
-        st.print('體重正常')
+        st.write('體重正常')
     elif (bmi < 27):
-        st.print('體重過重')
+        st.write('體重過重')
     else:
-        st.print('體重肥胖')
+        st.write('體重肥胖')
